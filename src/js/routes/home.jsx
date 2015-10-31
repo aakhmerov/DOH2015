3efbe09330dc4404664cbaef3a6877d6eebdcf36
Greg Smirnoff <gregsm1988@gmail.com>
2015-10-31 21:49:44 +0100

    header styles

--- src/js/routes/home.jsx
+++ src/js/routes/home.jsx
@@ -27,7 +27,7 @@ var HomePage = React.createClass({
     render: function() {
       var instance = this.state;
       return (<div className="col-md-offset-3 col-md-6">
-        <Header/>
+        <Header active="home"/>
             <hr/>
             <DietSelection diets={this.state.diets}/>
             <hr/>
