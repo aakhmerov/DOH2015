efeba7c809c35a0268e6d826fa9d5d6b06ca6126
Greg Smirnoff <gregsm1988@gmail.com>
2015-10-31 10:47:26 +0100

    backend init

--- dev/tasks/common.js
+++ dev/tasks/common.js
@@ -1,9 +1,10 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-connect-proxy');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-webpack');
-    grunt.loadNpmTasks('grunt-react');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-connect-proxy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-react');
+  grunt.loadNpmTasks('grunt-concurrent');
 };
