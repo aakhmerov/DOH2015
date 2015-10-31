86d43201e42d7d9dea1a5002177adebf583428ee
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-10-31 18:51:34 +0100

    Updated style

--- src/js/components/dietSelection.jsx
+++ src/js/components/dietSelection.jsx
@@ -8,11 +8,19 @@ var DietSelection = React.createClass({
     render: function() {
       var instance = this.state;
       return (<div className="row">
+      	<h3>Diet</h3>
 	    <ul className="col-md-offset-2 col-md-8">
 	        <li className="col-xs-4 col-sm-3 col-md-3 col-lg-2 col-xl-2">
 	            <div className="blocks">
 	                <div className="blocks-height"></div>
-	                <div className="blocks-layer img-fruit"></div>
+	                <div className="blocks-layer img-diet img-fruit"></div>
+	                <div className="blocks-layer-toggle"></div>
+	            </div>
+	        </li>
+	        <li className="col-xs-4 col-sm-3 col-md-3 col-lg-2 col-xl-2">
+	            <div className="blocks">
+	                <div className="blocks-height"></div>
+	                <div className="blocks-layer img-diet img-meat"></div>
 	            </div>
 	        </li>
 	    </ul>
