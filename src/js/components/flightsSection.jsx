a153fa7d58588afd5d6836d305240ea214255c48
Askar Akhmerov <askar.akhmerov@smava.de>
2015-11-01 02:19:29 +0100

    add basic flight card styling

--- src/js/components/flightsSection.jsx
+++ src/js/components/flightsSection.jsx
@@ -1,18 +1,31 @@
 /** @jsx React.DOM */
 var React = require('react');
+var moment = require('moment');
 
 var FlightItemWrapper = React.createClass({
     getInitialState: function () {
         return this.props;
     },
     render : function () {
+        var d1 = moment(this.props.flight.outboundFlight.departureDateTime).format('YYYY/MM/DD');
+        var t1 = moment(this.props.flight.outboundFlight.departureDateTime).format('HH:mm');
+        var d2 = moment(this.props.flight.outboundFlight.arrivalDateTime).format('YYYY/MM/DD');
+        var t2 = moment(this.props.flight.outboundFlight.arrivalDateTime).format('HH:mm');
         return (
-            <li className="col-xs-12">
-                <div className="">
-                    <div className=""></div>
-                    <div>{this.props.flight.outboundFlight.id}</div>
+            <div className="col-xs-12 cardRow">
+                <div className="card">
+                    <div className="row">
+                        <div className="col-xs-12 text-right">{this.props.flight.outboundFlight.id}</div>
+                    </div>
+                    <div className="row ">
+                        <div className="col-xs-4">{d1}</div>
+                    </div>
+                    <div className="row ">
+                        <div className="col-xs-4">{t1}-{t2}</div>
+                    </div>
+
                 </div>
-            </li>
+            </div>
             );
     }
 });
@@ -26,16 +39,16 @@ var FlightsSelection = React.createClass({
         if (this.props.flights) {
             flightItems = [];
             this.props.flights.map (function (flight,i) {
-                flightItems.push( <FlightItemWrapper flight={flight} key={i}/>)
+                flightItems.push( <FlightItemWrapper locales={['en-US']} flight={flight} key={i}/>)
             });
         } else {
             flightItems = '';
         }
         return (<div className="row">
             <h3>Flights</h3>
-            <ul className="row">
+            <div className="row">
             {flightItems}
-            </ul>
+            </div>
         </div>);
     }
 
