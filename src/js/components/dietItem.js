c6126b2269d00179bcee628c292427bcdac529f9
Vogeltak <vogeltak@gmail.com>
2015-11-01 11:58:54 +0100

    improve diet icons add more responsiveness and change hover animation

--- src/js/components/dietItem.js
+++ src/js/components/dietItem.js
@@ -24,7 +24,7 @@ var DietItem = React.createClass({
       classes = "blocks-layer img-diet img-"+ this.props.diet + " disabled";
       this.props.updateFilter(this.props.diet);
     }
-    return (<li className="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1" onClick={this.handleClick}>
+    return (<li className="col-xs-2 col-sm-1 col-md-1 col-lg-1 col-xl-1" onClick={this.handleClick}>
       <div className="blocks">
         <div className="blocks-height"></div>
         <div className={classes}></div>
